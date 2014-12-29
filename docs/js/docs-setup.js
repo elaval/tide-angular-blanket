871570b640c664f2625a3f00ca5613b8c7d216e5
Ernesto Laval <ernesto.laval@gmail.com>
2014-12-29 14:15:52 +0000

    Doc changes

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -10,7 +10,16 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "tide-angular",
       "shortDescription": "Generates charts to represent frequency of subjects with certain score (x attribute)",
-      "keywords": "api array attribute axis bubbles categories category chart charts codes color configuration data define defining directive display displayed div element entities frequency function generates graph height indicates left legend margin maximum minimun object objects options point populating regression represent return returns scale score selected set shoudl size sqr subjects tdcolorattribute tdcolorlegend tddata tdidattribute tdmaxsize tdminsize tdoptions tdselected tdsizeattribute tdsqrscalex tdtooltipmessage tdtrendline tdwidth tdxattribute tdyattribute teh text tide-angular tooltip trendline unique values wether widht"
+      "keywords": "api array asistencia attribute axis bars bubbles categories category chart charts charttype consider controller curves data de define defining describes directive displayed div example frequency function generates grupo html js label objects order parallel populating receives represent returns rigth score service simce size sort sortcriteria stacked string subjects td-blanket td-category-attribute td-chart-type td-data td-size-attribute td-sort-criteria td-x-attribute td-x-label td-y-label tdcategoryattribute tdcharttype tddata tdsizeattribute tdsortcriteria tdxattribute tdxlabel tdylabel tide-angular type values variable"
+    },
+    {
+      "section": "api",
+      "id": "tide-angular.service:tdBlanketLayout",
+      "shortName": "tdBlanketLayout",
+      "type": "service",
+      "moduleName": "tide-angular",
+      "shortDescription": "Helper service to calculate coordinates for set of area shapes that represent an histogram of ",
+      "keywords": "amount amountattribute angular api area attribute basey calculate category categoryattribute coordinates data directive dy example format function helper histogram js layout measurement measurementattribute module nodes output represent service set shapes subjects tdblanket tdblanketlayout tide-angular values var"
     }
   ],
   "apis": {
