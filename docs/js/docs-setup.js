45d9c6e96204eb7bf503b1cf466719a5938508fb
Ernesto Laval <ernesto.laval@gmail.com>
2014-12-29 08:45:27 +0000

    Add ngdocs documentation

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,26 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "tide-angular.directive:tdBlanket",
+      "shortName": "tdBlanket",
+      "type": "directive",
+      "moduleName": "tide-angular",
+      "shortDescription": "Generates charts to represent frequency of subjects with certain score (x attribute)",
+      "keywords": "api array attribute axis bubbles categories category chart charts codes color configuration data define defining directive display displayed div element entities frequency function generates graph height indicates left legend margin maximum minimun object objects options point populating regression represent return returns scale score selected set shoudl size sqr subjects tdcolorattribute tdcolorlegend tddata tdidattribute tdmaxsize tdminsize tdoptions tdselected tdsizeattribute tdsqrscalex tdtooltipmessage tdtrendline tdwidth tdxattribute tdyattribute teh text tide-angular tooltip trendline unique values wether widht"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "tide-angular-blanket.js"
+  ]
+};
